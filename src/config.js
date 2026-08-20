8dbe260f40f095a238e3845857076863a1dad214
fadefa88 <62143875+fadefa88@users.noreply.github.com>
2026-08-20 22:40:32 +0200

    Add finance configuration

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const INVESTMENT_CATEGORY = 'investimenti';
+
+// Classificazione provvisoria: si può modificare qui senza toccare i grafici.
+export const FIXED_CATEGORIES = new Set([
+  'mutuo',
+  'bollette',
+  'asilo',
+  'pulizie',
+  'assicurazione',
+  'spese condominiali',
+]);
+
+export const TARGET_SAVINGS_RATE = 0.20;
+
+export const MONTHS = [
+  'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+  'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre',
+];
+
+export const COLORS = {
+  primary: '#118DFF',
+  secondary: '#12239E',
+  orange: '#E66C37',
+  purple: '#6B007B',
+  pink: '#E044A7',
+  violet: '#744EC2',
+  yellow: '#D9B300',
+  red: '#D64554',
+  teal: '#197278',
+  green: '#1AAB40',
+  cyan: '#15C6F4',
+};
